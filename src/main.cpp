a7dbaad042e1ef17216eec46dd0f773d0e9a889e
salgue441 <carlossalguero441@gmail.com>
2023-06-11 16:44:42 -0600

    Implemented new tailored node class for the automatas

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,111 @@
 
 // C++ Standard Libraries
 #include <iostream>
+#include <string>
+#include <stack>
+#include <unordered_map>
 
+// Project files
+#include "./node/node.h"
+
+// Function prototypes
+std::string infix_to_postfix(const std::string &);
+
+/**
+ * @brief
+ * Main function of the program
+ * @param argc Number of arguments
+ * @param argv Arguments
+ * @return int Exit code
+ */
 int main(int argc, char const **argv)
 {
-    std::cout << "Hello World!" << std::endl;
+    Node node1(1);
+    Node node2(2);
+    Node node3(3);
+
+    // Add transitions
+    node1.add_transition('a', std::make_shared<Node>(node2));
+    node1.add_transition('b', std::make_shared<Node>(node3));
+
+    // Test get_target_id function
+    std::optional<int> targetIdA = node1.get_target_id('a');
+
+    if (targetIdA.has_value())
+        std::cout << "Target ID for transition 'a': "
+                  << targetIdA.value() << std::endl;
+
+    else
+        std::cout << "Transition 'a' does not exist." << std::endl;
+
+    std::optional<int> targetIdB = node1.get_target_id('b');
+    if (targetIdB.has_value())
+        std::cout << "Target ID for transition 'b': "
+                  << targetIdB.value() << std::endl;
+
+    else
+        std::cout << "Transition 'b' does not exist." << std::endl;
+
+    std::optional<int> targetIdC = node1.get_target_id('c');
+    if (targetIdC.has_value())
+        std::cout << "Target ID for transition 'c': "
+                  << targetIdC.value() << std::endl;
+
+    else
+        std::cout << "Transition 'c' does not exist." << std::endl;
+}
+
+/**
+ * @brief
+ * Converts an infix regular expression to a postfix regular expression
+ * @param infix Infix regular expression
+ * @return std::string Postfix regular expression
+ */
+std::string infix_to_postfix(const std::string &infix)
+{
+    std::unordered_map<char, int> precedence{
+        {'*', 3}, {'.', 2}, {'|', 1}, {'(', 0}};
+
+    std::string postfix;
+    std::stack<char> operators;
+
+    for (const char &character : infix)
+    {
+        if (std::isalpha(character))
+            postfix += character;
+
+        else if (character == '(')
+            operators.push(character);
+
+        else if (character == ')')
+        {
+            while (operators.top() != '(')
+            {
+                postfix += operators.top();
+                operators.pop();
+            }
+
+            operators.pop();
+        }
+
+        else
+        {
+            while (!operators.empty() &&
+                   precedence[operators.top()] >= precedence[character])
+            {
+                postfix += operators.top();
+                operators.pop();
+            }
+
+            operators.push(character);
+        }
+    }
+
+    while (!operators.empty())
+    {
+        postfix += operators.top();
+        operators.pop();
+    }
+
+    return postfix;
 }
---
+++ src/node/node.h
@@ -0,0 +1,60 @@
+/**
+ * @file node.h
+ * @author Carlos Salguero
+ * @brief Declaration of the node class
+ * @version 0.1
+ * @date 2023-06-11
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#ifndef SRC_NODE_NODE_H_
+#define SRC_NODE_NODE_H_
+
+#include <unordered_map>
+#include <optional>
+#include <memory>
+#include <vector>
+
+/**
+ * @class Node
+ * @brief
+ * Class that represents a node in the automata's graph
+ */
+class Node
+{
+public:
+    // Character and target node ID
+    using Transition = std::unordered_map<char, std::shared_ptr<Node>>;
+
+    // Constructors
+    Node() = default;
+    explicit Node(int id, bool is_final = false);
+
+    // Destructor
+    ~Node() = default;
+
+    // Access Methods
+    int get_id() const;
+    bool is_final() const;
+    const Transition &get_transitions() const;
+
+    // Modifiers
+    void set_id(const int &);
+    void set_final(const bool &);
+    void add_transition(const char &, const std::shared_ptr<Node> &);
+
+    // Operators
+    bool operator==(const Node &) const;
+    bool operator!=(const Node &) const;
+
+    // Methods
+    std::optional<int> get_target_id(const char &) const;
+
+private:
+    int m_id;
+    Transition m_transitions;
+    bool m_is_final;
+};
+
+#endif //! SRC_NODE_NODE_H_
